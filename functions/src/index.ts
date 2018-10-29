2621166175a1cf6eaa163291e8e1dca2c592a88a
Sangarllo <santiago.garcia.llorente@gmail.com>
2018-10-29 17:30:57 +0100

    Improve generateMini function to get thumbs of 400x400

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@
 // https://firebase.google.com/docs/functions/typescript
 
 import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 import * as Storage from '@google-cloud/storage';
 const gcs = new Storage();
@@ -36,21 +38,58 @@ export const generateMini = functions.storage
       destination: tmpFilePath
     });
 
+    // 2b. Gets the metadata for the file
+    const [metadata] = await bucket.file(filePath).getMetadata();
+    console.log(`File: ${metadata.name}`);
+    const uploadId = metadata.name.substr(5, 36);
+
+    console.log(`uploadId: ${uploadId}`);
+
+    const contType = metadata.contentType;
+    const fileExtension = contType.replace('image/', '.');
+    const thumbName = `mini@${fileName}${fileExtension}`;
+
     // 3. Resize the images and define an array of upload promises
-    const thumbName = `mini@${fileName}`;
     const thumbPath = join(workingDir, thumbName);
-    const SIZE_MINI = 200;
+    const SIZE_MINI = 400;
 
     // Resize source image
     await sharp(tmpFilePath)
-        .resize(SIZE_MINI, SIZE_MINI)
+        .resize(SIZE_MINI, SIZE_MINI, {
+          withoutEnlargement: true
+        })
         .toFile(thumbPath);
 
     // 4. Cleanup remove the tmp/thumbs from the filesystem
     fs.remove(workingDir);
 
+    const customMetadata = {
+      app: 'Memento-App-Universal',
+      uploadId: metadata.name,
+      type: 'MiniSize'
+    };
+
     // Upload to GCS
-    return bucket.upload(thumbPath, {
-        destination: join(bucketDir, thumbName)
+    const destThumb = join(bucketDir, thumbName);
+
+    return bucket.upload(
+      thumbPath, {
+        destination: destThumb,
+        metadata: customMetadata
+    })
+    .then((data) => {
+
+      const file = data[0];
+
+      const dataDB = {
+        uploadId: uploadId,
+        originalSrc: destThumb
+      };
+
+      const db = admin.firestore();
+      const settings = {/* your settings... */ timestampsInSnapshots: true};
+      db.settings(settings);
+      db.collection('images').doc(uploadId).set(dataDB);
+      console.log(`modificada ${uploadId}`);
     });
-  });
+});
